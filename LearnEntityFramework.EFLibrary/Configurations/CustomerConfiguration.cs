6c42628ad70da584e4d9e2b0afea6a1c6fa8f4b7
Kennedi Nadeak <kennedinadeak235@gmail.com>
2023-01-09 21:37:44 +0700

    Configuration EF Entities

---
+++ LearnEntityFramework.EFLibrary/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using LearnEntityFramework.EFLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearnEntityFramework.EFLibrary.Configurations
+{
+    internal sealed class CustomerConfiguration : IEntityTypeConfiguration<CustomerEntity>
+    {
+        public void Configure(EntityTypeBuilder<CustomerEntity> builder)
+        {
+            builder.ToTable("customer");
+
+            builder.HasKey(customer => customer.Id);
+
+            builder.Property(customer => customer.Id)
+                   .IsRequired()
+                   .HasColumnName("id");
+
+            builder.Property(customer => customer.FirstName)
+                   .IsRequired()
+                   .HasColumnType("varchar")
+                   .HasColumnName("first_name");
+
+            builder.Property(customer => customer.LastName)
+                   .HasColumnType("varchar(50)")
+                   .HasMaxLength(50)
+                   .IsFixedLength()
+                   .HasColumnName("last_name");
+
+
+        }
+    }
+}
--- LearnEntityFramework.EFLibrary/Entities/CustomerEntity.cs
+++ LearnEntityFramework.EFLibrary/Entities/CustomerEntity.cs
@@ -1,8 +1,13 @@
-﻿namespace LearnEntityFramework.EFLibrary.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LearnEntityFramework.EFLibrary.Entities
 {
     public class CustomerEntity
     {
         public int Id { get; set; }
-        public string? FirstName { get; set; }
+        [MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; }
     }
 }
